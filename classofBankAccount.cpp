e94b69f7d6dcd70a8f4d73c3304ae4ac79e281c5
Rushda Abbasi <rush.cs19@gmail.com>
2026-01-26 15:27:10 +0500

    Add files via upload

---
+++ classofBankAccount.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+ class BankAccount{
+   string owner;
+   double balance;
+
+	public:
+		BankAccount(string name, double initial) {
+			owner = name;
+			balance = initial;
+}
+
+	void deposit(double amount) {
+		if (amount > 0) balance += amount;
+}
+
+	bool withdraw(double amount) {
+		if (amount <= balance && amount > 0) {
+			balance -= amount;
+			return true;
+}
+return false;
+}
+void display() {
+cout << "Owner: " << owner << ", Balance: $" << balance << "\n";
+}
+};
+
+int main() {
+
+// Create two OBJECTS
+
+BankAccount acc1("Ali", 100.0);
+
+BankAccount acc2("Sara", 50.0);
+
+acc1.deposit(25);
+
+acc2.withdraw(10);
+
+acc1.display(); // Owner: Ali, Balance: $125
+
+acc2.display(); // Owner: Sara, Balance: $40
+
+// ? This would cause ERROR:
+
+// cout << acc1.balance; // 'balance' is private!
+
+}
