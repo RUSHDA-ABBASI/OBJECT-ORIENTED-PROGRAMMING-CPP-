3c1ae9507994d01e74960745577f376c0ef0d343
Rushda Abbasi <rush.cs19@gmail.com>
2026-03-12 19:37:04 +0500

    Add files via upload

---
+++ functionOverloadingExample.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class Print{
+    public:
+    void show(int x){
+        cout<< " integer : "<<x<<endl;
+    }
+    void show(char ch){
+        cout<< " character : "<<ch<<endl;
+    }
+};
+int main(){
+    Print p1;
+    p1.show(2);
+    p1.show('r');
+
+    return 0;
+}
